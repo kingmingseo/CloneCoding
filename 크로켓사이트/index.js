1dc078d9342f607cfcb6ad608030d7c6c01a01c8
kingmingseo <101965138+kingmingseo@users.noreply.github.com>
2024-01-12 11:19:13 +0900

    탭 기능 추가

--- 크로켓사이트/index.js
+++ 크로켓사이트/index.js
@@ -2,15 +2,15 @@
 /***************네비바 클릭 시 화면 전환******************** */
 let aTags = document.querySelectorAll('a');
 aTags.forEach(element => {
-  element.addEventListener('click',e=>{
+  element.addEventListener('click', e => {
     e.preventDefault();
     console.log(e)
     console.log(element)
     let target = document.querySelector(element.getAttribute('href'));
-    
+
     window.scrollTo({
-      'behavior' : 'smooth',
-      'top' : target.offsetTop
+      'behavior': 'smooth',
+      'top': target.offsetTop
     })
   });
 });
@@ -23,27 +23,52 @@ aTags.forEach(element => {
 /***************이미지 슬라이드 기능 구현******************** */
 
 let slider = document.querySelector('#slider');
-let slides =slider.querySelector('.slides');
+let slides = slider.querySelector('.slides');
 let slide = slides.querySelectorAll('.slide');
 
-let currentSlide = 0 ;
+let currentSlide = 0;
 
-setInterval(()=>{
-  let from = -(1024*currentSlide);
+setInterval(() => {
+  let from = -(1024 * currentSlide);
   let to = from - 1024;
   slides.animate({
-    marginLeft : [from + "px", to + "px"]
-  },{
-    duration:500,
-    easing :"ease",
-    iterations :1,
-    fill:"both"
+    marginLeft: [from + "px", to + "px"]
+  }, {
+    duration: 500,
+    easing: "ease",
+    iterations: 1,
+    fill: "both"
   });
-  currentSlide ++;
-  if(currentSlide === ( slide.length -1)){
+  currentSlide++;
+  if (currentSlide === (slide.length - 1)) {
     currentSlide = 0
   }
-},3000)
+}, 3000)
 
 //animate({properties}, {duration[필수],easing,iteration, fill} ) 함수 사용
 // marginLeft: [from , to] 형식으로 속성을 변화 시킴
+
+
+/***************tab 기능 구현******************** */
+let links = document.querySelectorAll('.tabs-list li a')
+let items = document.querySelectorAll('.tabs-list li')
+
+links.forEach((element) => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault();
+  })
+})
+
+
+items.forEach((element) => {
+  element.addEventListener('click', (e) => {
+    let tabId = element.querySelector('a').getAttribute('href');
+    document.querySelectorAll('.tabs-list li, .tabs div.tab').forEach((item) => {
+      item.classList.remove("active")
+    })
+    document.querySelector(tabId).classList.add("active");
+    element.classList.add("active");
+  })
+})
+
+
