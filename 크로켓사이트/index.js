7c001964779e625cea213a62ba676fdd629817a9
kingmingseo <101965138+kingmingseo@users.noreply.github.com>
2024-01-12 12:40:15 +0900

    클릭을 통한 이미지 슬라이드 구현

--- 크로켓사이트/index.js
+++ 크로켓사이트/index.js
@@ -72,3 +72,56 @@ items.forEach((element) => {
 })
 
 
+//****************클릭을 통한 이미지 슬라이드 기능 구현 ******************** */
+document.querySelector(".right-arrow").addEventListener('click',()=>{
+  var currentSlide = document.querySelector("#photo .slide.active");
+  var nextSlide = currentSlide.nextElementSibling;
+  if (nextSlide === null) {
+      nextSlide = currentSlide.parentElement.firstElementChild;
+  }
+      currentSlide.animate({
+      opacity: [1, 0]
+  }, {
+      duration: 500,
+      easing: "ease",
+      iterations: 1,
+      fill: "both"
+  });
+  currentSlide.classList.remove("active");
+  nextSlide.animate({
+      opacity: [0, 1]
+  }, {
+      duration: 500,
+      easing: "ease",
+      iterations: 1,
+      fill: "both"
+  });
+  nextSlide.classList.add("active");
+})
+
+
+document.querySelector(".left-arrow").addEventListener('click',()=>{
+  var currentSlide = document.querySelector("#photo .slide.active");
+  var previousSlide = currentSlide.previousElementSibling;
+  if (previousSlide === null) {
+    previousSlide = currentSlide.parentElement.lastElementChild;
+  }
+      currentSlide.animate({
+      opacity: [1, 0]
+  }, {
+      duration: 500,
+      easing: "ease",
+      iterations: 1,
+      fill: "both"
+  });
+  currentSlide.classList.remove("active");
+  previousSlide.animate({
+      opacity: [0, 1]
+  }, {
+      duration: 500,
+      easing: "ease",
+      iterations: 1,
+      fill: "both"
+  });
+  previousSlide.classList.add("active");
+})
